dc7bd0d4925d523d9b22dd4b37be0f2b4b6da308
Shannon Besson <shannon0413@outlook.com>
2025-04-03 15:01:15 +0200

    (feat(SessionContext):Ajout de SessionContext.tsx)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
+import { SessionProvider } from "../SessionContext";
 
 export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Accueil" }} />
       <Stack.Screen name="timer" options={{ title: "Chrono" }} />
+      <Stack.Screen name="sessions" options={{ title: "Séances" }} />
     </Stack>
   );
 }
